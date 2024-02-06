228d4bfb74f3367da5ef79d0fdd50bfd3029a5b8
Thiago Miguel da Costa <thiago-mig@hotmail.com>
2024-02-05 23:50:26 -0300

    Adicionando o arquivo de javascript ao projeto

---
+++ script.js
@@ -0,0 +1,13 @@
+const textInput = document.querySelector('textarea');
+const encryptBtn = document.querySelector('.encrypt');
+const outputContainerDefault = document.querySelector('.output-section__default');
+const outputContainerResult = document.querySelector('.output-section__result-container')
+const resultParagraph = document.querySelector('.result');
+
+const encrypt = () => {
+  let userText = textInput.value;
+
+};
+
+
+console.log(resultParagraph.classList.add('chupetex'));
