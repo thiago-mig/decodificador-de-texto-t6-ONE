bf9f74ddf436b0ee91b2607328c6753bccc27150
Thiago Miguel da Costa <thiago-mig@hotmail.com>
2024-02-06 00:59:55 -0300

    Realizado a função de encripitar texto e a função de exibir o resultado

--- script.js
+++ script.js
@@ -1,13 +1,50 @@
 const textInput = document.querySelector('textarea');
 const encryptBtn = document.querySelector('.encrypt');
 const outputContainerDefault = document.querySelector('.output-section__default');
-const outputContainerResult = document.querySelector('.output-section__result-container')
+const outputContainerResult = document.querySelector('.output-section__result')
 const resultParagraph = document.querySelector('.result');
 
-const encrypt = () => {
-  let userText = textInput.value;
 
+const encrypt = (value) => {
+  let encryptedText = '';
+  if (value.length !== 0) {
+    for (let letter of textInput.value) {
+      switch (letter) {
+        case 'a':
+            encryptedText += 'ai';
+            break;
+        case 'e':
+            encryptedText += 'enter';
+            break;
+        case 'i':
+            encryptedText += 'imes';
+            break;
+        case 'o':
+            encryptedText += 'ober';
+            break;
+        case 'u':
+            encryptedText += 'ufat';
+            break;
+        default:
+            encryptedText += letter;
+            break;
+      }
+    }
+    return encryptedText;
+  }  
 };
 
+const showResult = () => {
+  if(outputContainerResult.classList.contains('hidden')){
+    outputContainerDefault.classList.add('hidden');
+    outputContainerResult.classList.remove('hidden');
+  }
+  resultParagraph.innerText = encrypt(textInput.value);
+  textInput.value = '';
+}
 
-console.log(resultParagraph.classList.add('chupetex'));
+
+
+encryptBtn.addEventListener('click', () =>{
+    showResult();
+});
